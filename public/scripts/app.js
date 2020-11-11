1719d77b7095eee85b23899bfd415826fa17ab67
NurlybekSultamuratov <nsultamu@my.centennialcollege.ca>
2020-11-10 21:51:57 -0500

    dslfkdl

---
+++ public/scripts/app.js
@@ -0,0 +1,11 @@
+//INSIDE PUBLIC/SCRIPTS/APP.JS
+(function(){
+
+    function Start()
+    {
+        console.log("Project started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
