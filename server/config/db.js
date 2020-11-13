d842bf5299ef0a209cf1b066802a4f3e4d520db3
webfinalproject-group2 <74219383+webfinalproject-group2@users.noreply.github.com>
2020-11-13 17:30:02 -0500

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = 
 {
     "URI": "mongodb://localhost/book_store"//you need to add our db
-
+    "uri": "mongodb+srv://amish99:LecbrUWsoMCfs6uR@mongodbserver.hgnur.mongodb.net/book_store?retryWrites=true&w=majority"
 }
